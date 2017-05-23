f021ea196537797ab5d744b8fdba0f047f7141ac
agbilotia1998 <agbilotia1998@gmail.com>
2017-05-23 23:29:01 +0530

    Reformatted code

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,4 @@
-/*
-   
-    Template Name : Rolling - Freelancer Portfolio Template
-    Author : UiPasta Team
-    Website : http://www.uipasta.com/
-    Support : http://www.uipasta.com/support/
-	
-	
-*/
 
-
-
-/*
-   
-   Table Of Content
-   
-   1. Preloader
-   2. Smooth Scroll
-   3. Scroll Naviagation Background Change with Sticky Navigation
-   4. Mobile Navigation Hide or Collapse on Click
-   5. Scroll To Top
-   6. Typed.js
-   7. Parallax Background
-   8. Portfolio Filtering
-   9. Magnific Popup
-  10. Testimonial Carousel/Slider
-  11. Statistics Counter
-  12. Google Map
- 
-
-*/
 
 
 (function ($) {
@@ -36,16 +6,15 @@
 
     jQuery(document).ready(function () {
 
-        
-       /* Preloader */
-		
-        $(window).on('load', function() {
-          $('body').addClass('loaded');
+
+        /* Preloader */
+
+        $(window).on('load', function () {
+            $('body').addClass('loaded');
         });
-		
-		
-		
-       /* Smooth Scroll */
+
+
+        /* Smooth Scroll */
 
         $('a.smoth-scroll').on("click", function (e) {
             var anchor = $(this);
@@ -54,12 +23,10 @@
             }, 1000);
             e.preventDefault();
         });
-		
 
 
-       
-       /* Scroll Naviagation Background Change with Sticky Navigation */
-		 
+        /* Scroll Naviagation Background Change with Sticky Navigation */
+
         $(window).on('scroll', function () {
             if ($(window).scrollTop() > 100) {
                 $('.header-top-area').addClass('navigation-background');
@@ -67,12 +34,10 @@
                 $('.header-top-area').removeClass('navigation-background');
             }
         });
-		
-		
-		
-		
-       /* Mobile Navigation Hide or Collapse on Click */
-		
+
+
+        /* Mobile Navigation Hide or Collapse on Click */
+
         $(document).on('click', '.navbar-collapse.in', function (e) {
             if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
                 $(this).collapse('hide');
@@ -81,42 +46,38 @@
         $('body').scrollspy({
             target: '.navbar-collapse',
             offset: 195
-        
-		 });
-		 
-		
-		
-		
+
+        });
+
+
         /* Scroll To Top */
-		
-        $(window).scroll(function(){
-        if ($(this).scrollTop() >= 500) {
-            $('.scroll-to-top').fadeIn();
-         } else {
-            $('.scroll-to-top').fadeOut();
-         }
-	   });
-	
-	
-	    $('.scroll-to-top').click(function(){
-		  $('html, body').animate({scrollTop : 0},800);
-		  return false;
-	    });
-		
-		
-		
-		
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() >= 500) {
+                $('.scroll-to-top').fadeIn();
+            } else {
+                $('.scroll-to-top').fadeOut();
+            }
+        });
+
+
+        $('.scroll-to-top').click(function () {
+            $('html, body').animate({scrollTop: 0}, 800);
+            return false;
+        });
+
+
         /* Typed.js */
-		
-        $(window).load(function(){
-        $(".typing").typed({
-            strings: ["I am a full stack developer.", "I Live in Allahabad."],    /* You can change the home section typing text from
-	                                                                                            here and do not use "&" use "and" */
-            typeSpeed: 50
-          });
-         });
-        
-		 
+
+        $(window).load(function () {
+            $(".typing").typed({
+                strings: ["I am a full stack developer.", "I Live in Allahabad."], /* You can change the home section typing text from
+                 here and do not use "&" use "and" */
+                typeSpeed: 50
+            });
+        });
+
+
         /* Parallax Background */
 
         $(window).stellar({
@@ -127,68 +88,56 @@
             verticalOffset: 0,
         });
 
-        
-		
-		
+
         /* Portfolio Filtering */
 
         $('.portfolio-inner').mixItUp();
 
 
-       
         /* Magnific Popup */
 
         $('.portfolio-popup').magnificPopup({
             type: 'image',
-			
-            gallery: { enabled: true },
-			zoom: { enabled: true,
-			        duration: 500
-					
-          },
-		  
-         image:{
-               markup: '<div class="mfp-figure portfolio-pop-up">'+
-               '<div class="mfp-close"></div>'+
-               '<div class="mfp-img"></div>'+
-               '<div class="mfp-bottom-bar portfolio_title">'+
-               '<div class="mfp-title"></div>'+
-               '<div class="mfp-counter"></div>'+
-               '</div>'+
-               '</div>',
 
-               titleSrc:function(item){
-                return item.el.attr('title');
-              }
+            gallery: {enabled: true},
+            zoom: {
+                enabled: true,
+                duration: 500
+
+            },
+
+            image: {
+                markup: '<div class="mfp-figure portfolio-pop-up">' +
+                '<div class="mfp-close"></div>' +
+                '<div class="mfp-img"></div>' +
+                '<div class="mfp-bottom-bar portfolio_title">' +
+                '<div class="mfp-title"></div>' +
+                '<div class="mfp-counter"></div>' +
+                '</div>' +
+                '</div>',
+
+                titleSrc: function (item) {
+                    return item.el.attr('title');
+                }
             }
-		  
-		  
-          });
-
-       
-	   
-		 
-        /*
-		
-		
-		
-        /* Statistics Counter */
-		
-        $('.statistics').appear(function() {
-           var counter = $(this).find('.statistics-count');
-           var toCount = counter.data('count');
-      
-           $(counter).countTo({
-           from: 0,
-           to: toCount,
-           speed: 5000,
-           refreshInterval: 50
-           })
-           });
-		   
-		  
 
 
-            });
+        });
 
-   })(jQuery);
+
+        $('.statistics').appear(function () {
+            var counter = $(this).find('.statistics-count');
+            var toCount = counter.data('count');
+
+            $(counter).countTo({
+                from: 0,
+                to: toCount,
+                speed: 5000,
+                refreshInterval: 50
+            })
+        });
+
+
+    });
+
+})(jQuery);
